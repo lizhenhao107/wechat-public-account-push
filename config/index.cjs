2657ac035953c1ceaf9eac3b72a03076803d44e7
lizhenhao <1073425329@qq.com>
2022-11-23 14:32:18 +0800

    修改配置

--- config/index.cjs
+++ config/index.cjs
@@ -92,7 +92,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'omNyR6qG891V0G-Yp7kzA3wOmhR4',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'S1GsnDFKYlSxXshTNq6bA6KYmtMsl3xrPVeRyyF6fFE',
+      useTemplateId: 'BBpai2ZFjWIKO77oAT_Gb-fyA8CJTg1lyMBdCkjQhTk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       openUrl: 'https://yufu.pub/',
@@ -123,7 +123,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'omNyR6mB3evvao16vLAF5sPPQd4o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'S1GsnDFKYlSxXshTNq6bA6KYmtMsl3xrPVeRyyF6fFE',
+      useTemplateId: 'BBpai2ZFjWIKO77oAT_Gb-fyA8CJTg1lyMBdCkjQhTk',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '03-07',
       openUrl: 'https://yufu.pub/',
